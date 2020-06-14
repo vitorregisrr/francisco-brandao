24b02a7623fc8ce2d4392dc98b29a38fcd35beac
vitorregisr <vitorregisrr@gmail.com>
2020-06-13 23:37:36 -0300

    Update brushs and add e-mail to footer

--- src/components/Sections/Footer/index.js
+++ src/components/Sections/Footer/index.js
@@ -6,6 +6,10 @@ const Footer = (props) => {
     return (
         <footer className="Footer wow fadeInUp">
            <h3 className="Footer__brand">Chico Brandão</h3>
+           <p className="Footer__email">
+           contato@chiquinhobrandao.com.br
+           </p>
+           <hr/>
            <p className="Footer__copy">
            © 2020 Chiquinho Brandão. Todos os direitos reservados.
            </p>

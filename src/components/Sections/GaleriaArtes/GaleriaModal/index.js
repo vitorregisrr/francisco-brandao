0a072ae7fa6bfd5352e247d1ea436d2910e9c28b
vitorregisr <vitorregisrr@gmail.com>
2020-06-18 15:17:07 -0300

    Fix pagination

--- src/components/Sections/GaleriaArtes/GaleriaModal/index.js
+++ src/components/Sections/GaleriaArtes/GaleriaModal/index.js
@@ -19,15 +19,7 @@ const GaleriaModal = ({currentItem, items, closeModal, visible}) => {
     const slickRef = useRef(false);
 
     useEffect(() => {
-        if (currentItem && items) {
-            const currentIndex = $(`.slick-slider [data-id="${currentItem}"]`)
-                .parent()
-                .parent()
-                .attr('data-index');
-            slickRef
-                .current
-                .slickGoTo(currentIndex);
-        }
+       
     }, [currentItem])
 
     return (
@@ -37,24 +29,7 @@ const GaleriaModal = ({currentItem, items, closeModal, visible}) => {
                 className="GaleriaModal__close"
                 title="Fechar modal"></button>
             <div className="GaleriaModal__content">
-                {items
-                    ? <Slick ref={slickRef} {...slickSettings}>
-                            {items.map(i => (
-                                <article className="GaleriaModal__item" data-id={i.id}>
-                                    <img className="GaleriaModal__item-img" src={i.file}/>
-                                    <h4 className="GaleriaModal__item-nome">{i.file_desc}</h4>
-                                    <h5 className="GaleriaModal__item-autor">{i.file_artista}</h5>
-
-                                    {/* Apenas se houver descrição */}
-                                    {/* (i.file_desc2
-                                        ? <React.Fragment><hr className="GaleriaModal__item-line"/>
-                                                <p className="GaleriaModal__item-desc">{i.file_desc2}</p>
-                                            </React.Fragment>
-                                    : null */}
-                                </article>
-                            ))}
-                        </Slick>
-                    : null}
+               
             </div>
         </aside>
     )

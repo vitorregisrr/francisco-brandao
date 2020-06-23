9d842e8d7923df379047707f9fc0a4ea24349007
Rafhael Payão <51467263+rafaeltmbr@users.noreply.github.com>
2020-06-23 17:42:01 -0300

    changed axios baseURL to final api url

--- src/axios.instance.js
+++ src/axios.instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://franb.sitesemproducao.com'
+    baseURL: 'https://api.chiquinhobrandao.com.br'
 });
 
 export default instance;

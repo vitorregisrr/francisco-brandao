bb1d6d770fa3e419ead739c9baf37c7b876c099b
Rafhael Payão <51467263+rafaeltmbr@users.noreply.github.com>
2020-06-23 12:13:26 -0300

    changed api baseURL to https instead of http

--- src/axios.instance.js
+++ src/axios.instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://franb.sitesemproducao.com'
+    baseURL: 'https://franb.sitesemproducao.com'
 });
 
 export default instance;
